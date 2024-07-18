ae60b157c07f83a0e0a4fc63ea157fd4087ee50a
Insight_Horizon <54343240+Ins1ght32@users.noreply.github.com>
2024-07-18 23:26:13 +0800

    new

--- src/test/java/mycompany/app/AppSecondaryTest.java
+++ src/test/java/mycompany/app/AppSecondaryTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
-public class AppTest2 {
+public class AppSecondaryTest {
     private WebDriver driver;
 
     @Before
